b649196b3ec0cdf7af94aeb64a9779a7b5fe68a8
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-03-27 16:11:01 -0400

    Create operate function
    Takes an operator and 2 numbers and then calls
    one of the basic operator functions on the numbers
    This function will be used to update the display

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function multiply(x, y){
 function divide(x, y){
     return (x / y);
 };
+
+//Operate function that will take input and call back a basic math operator function
+function operate(x, operator, y){
+    switch(operator){
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+        default:
+            return 'Please use a correct operator'
+    }
+}
