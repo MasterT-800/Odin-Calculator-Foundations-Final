1776933bed25903aad62f8f31f522ac0cd2e36a8
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-03-30 10:54:01 -0400

    Made the calcukator work
    When buttons are pushed on html doc they are
    stored in a variable which are then used to call
    the operate function. The solution is then
    displayed on the display paragraph above the
    buttons.

--- script.js
+++ script.js
@@ -38,6 +38,44 @@ const display = document.querySelector('p');
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener("click", () => {
+        if(button.innerText == 'clear'){
+            return display.innerText = '';
+            
+        }
+        if (button.innerText == '='){
+            //Turn user input into an array so we can iterate over it
+            let input = display.innerText.split('');
+            let length = input.length;
+            let x = 0;
+            let y = 0;
+            let operator;
+            console.log(input);
+            //Iterate over array to find x, y, and operator variables
+            for (let i=0; i <= length; i++){
+                if (input[i] == '+' ||
+                    input[i] == '-' ||
+                    input[i] == '*' ||
+                    input[i] == '/'){
+                        operator = input[i];
+                        for(let j= i+1; j < length; j++){
+                            y+= input[j];
+                        }
+                    break;
+                    }
+                x += input[i];
+                console.log(x);
+            }
+            x = Number(x);
+            y = Number(y);
+            console.log(x);
+            console.log(typeof(x));
+            console.log(y);
+            console.log(typeof(y));
+            console.log(operator);
+            let solution = operate(x, operator, y);
+            return display.innerText = solution;
+        }
+        
       display.innerText += (button.innerText);
     });
   });
